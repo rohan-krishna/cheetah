3da10497488235c604550f7e0f4e76dd828772b5
Rohan Krishna <Rohan.Krishna@csscorp.com>
2017-08-08 14:09:11 +0530

    Semantic UI Added

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/semantic-ui.css') }}">
 </head>
 <body>
     <div id="app">
@@ -75,6 +76,8 @@
     </div>
 
     <!-- Scripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/semantic-ui.js') }}"></script>
 </body>
 </html>
